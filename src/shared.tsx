099ea1d640e29a52e2a6b209e70dfc7c5c8a0ad9
Rahel Lüthy <rahel.luethy@gmail.com>
2019-09-18 15:42:45 +0200

    #12: Fix range to follow start inclusive, end exclusive semantics

--- src/shared.tsx
+++ src/shared.tsx
@@ -10,5 +10,8 @@ export const defaultDarkGrey = grey['800']
 export const selectionColor = 'rgba(255, 255, 141, 0.5)' // transparent yellow A100
 export const selectionColorOpaque = yellow.A100
 
+/**
+ * Returns an array of numbers from start (inclusive) to end (exclusive)
+ */
 export const range = (start: number, end: number) =>
-    new Array(end - start + 1).fill(undefined).map((_v_, index) => index + start)
+    new Array(end - start).fill(undefined).map((_v_, index) => index + start)
