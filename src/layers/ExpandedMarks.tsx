7d6ed8fcf4499f6caadbfd8a07e4ea3c4d095485
protyze <jan.azzati@gmail.com>
2022-05-18 11:30:50 +0200

    Fix typing issue

--- src/layers/ExpandedMarks.tsx
+++ src/layers/ExpandedMarks.tsx
@@ -16,36 +16,34 @@ interface Props<EID extends string, LID extends string, E extends TimelineEvent<
   onEventClick?: (eventId: EID) => void
 }
 
-export const ExpandedMarks = React.memo(
-  <EID extends string, LID extends string, E extends TimelineEvent<EID, LID>>({
-    height,
-    events,
-    lanes,
-    timeScale,
-    yScale,
-    eventComponent,
-    onEventHover,
-    onEventUnhover,
-    onEventClick,
-  }: Props<EID, LID, E>) => {
-    const marks = lanes.map((lane: TimelineLane<LID>) => {
-      const laneSpecificEvents = events.filter((e) => e.laneId === lane.laneId)
-      return (
-        <g key={`marks-${lane.laneId}`}>
-          <Marks
-            height={height}
-            events={laneSpecificEvents}
-            timeScale={timeScale}
-            y={yScale(lane.laneId)!}
-            eventComponent={eventComponent}
-            onEventHover={onEventHover}
-            onEventUnhover={onEventUnhover}
-            onEventClick={onEventClick}
-          />
-        </g>
-      )
-    })
+export const ExpandedMarks = <EID extends string, LID extends string, E extends TimelineEvent<EID, LID>>({
+  height,
+  events,
+  lanes,
+  timeScale,
+  yScale,
+  eventComponent,
+  onEventHover,
+  onEventUnhover,
+  onEventClick,
+}: Props<EID, LID, E>) => {
+  const marks = lanes.map((lane: TimelineLane<LID>) => {
+    const laneSpecificEvents = events.filter((e) => e.laneId === lane.laneId)
+    return (
+      <g key={`marks-${lane.laneId}`}>
+        <Marks
+          height={height}
+          events={laneSpecificEvents}
+          timeScale={timeScale}
+          y={yScale(lane.laneId)!}
+          eventComponent={eventComponent}
+          onEventHover={onEventHover}
+          onEventUnhover={onEventUnhover}
+          onEventClick={onEventClick}
+        />
+      </g>
+    )
+  })
 
-    return <g>{marks}</g>
-  }
-)
+  return <g>{marks}</g>
+}
