e82a04d862bac7007e7420f3721e69bcdf820404
protyze <jan.azzati@gmail.com>
2022-11-16 10:02:53 +0100

    Provide output globals

--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,13 @@ export default defineConfig({
     },
     rollupOptions: {
       external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'jsxRuntime',
+        },
+      },
     },
   },
   test: {
