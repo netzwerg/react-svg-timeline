8106f70de9732f1beab7ca458052adb895645f65
protyze <jan.azzati@gmail.com>
2022-11-16 10:01:08 +0100

    Satisfy eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
 import { resolve } from 'path'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import checker from 'vite-plugin-checker'
 
